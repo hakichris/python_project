d2ad71df6ed5fde05b811acdc1fd54f290ea85a2
Chris <hakizachrist1@gmail.com>
2023-08-24 22:55:32 +0200

    Update home.html

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+posts = [
+
+  {
+      'author': 'chris',
+      'title': 'Love of my life',
+      'content': 'My love story started when I was 6',
+      'date_posted': 'August 6,2018'
+  },
+
+
+  {
+      'author': 'chris',
+      'title': 'Love of my life',
+      'content': 'My love story started when I was 6',
+      'date_posted': 'August 6,2018'
+  },
+
+
+  {
+      'author': 'chris',
+      'title': 'Love of my life',
+      'content': 'My love story started when I was 6',
+      'date_posted': 'August 6,2018'
+  }
+
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
