a33f2048a3f29d9e0985d543b3539718dc21b3d6
Chris <hakizachrist1@gmail.com>
2023-10-03 23:28:02 +0200

    Update register

--- pythonblog/settings.py
+++ pythonblog/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'crispy_forms',
+    "crispy_bootstrap5",
     'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap5',
+    
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- users/views.py
+++ users/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+from .forms import UserRegisterForm
+ 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-         form = UserCreationForm(request.POST)
-         print("at least i got here")
+         form = UserRegisterForm(request.POST)
          if form.is_valid():
-              print("yes the form is validated")
+              form.save()
               username = form.cleaned_data.get('username')
               messages.success(request, f'Acccount created for {username}!')
               return redirect('blog-home')
     else:
-        print("i am here, I did not make it")
-        form = UserCreationForm()
+        form = UserRegisterForm()
        
     return render(request, 'users/register.html', {'form': form})
  
