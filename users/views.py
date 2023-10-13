448bd7d2da5aa17ee21523d0e06e241b5fb5d9f7
Chris <hakizachrist1@gmail.com>
2023-10-14 00:07:07 +0200

    Update forms

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
  
 # Create your views here.
 def register(request):
@@ -18,5 +19,13 @@ def register(request):
 
 @login_required
 def profile(request): 
+     u_form = UserUpdateForm()
+     p_form = ProfileUpdateForm()
+
+     context = {
+          'u_form' : u_form,
+          'p_form' : p_form
+     }
+     
      return render(request, 'users/profile.html')
  
