97ed525fe7aac1b06f3e40845816c22b65ee9d7c
Chris <hakizachrist1@gmail.com>
2023-10-14 00:22:36 +0200

    Update profile forms

--- users/forms.py
+++ users/forms.py
@@ -10,7 +10,7 @@ class UserRegisterForm(UserCreationForm):
        model = User
        fields = ['username', 'email', 'password1', 'password2']
     
-class UseUpdateForm(forms.ModelForm):
+class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
     class Meta: 
