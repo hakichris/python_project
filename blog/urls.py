47c73cd71a06370d81cc46ff50f37b618b1dce7f
Chris <hakizachrist1@gmail.com>
2023-10-24 23:59:27 +0200

    update postdetail

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
-    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'), 
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DateDetailView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -18,8 +18,9 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
-class PostDetailView(DateDetailView): 
+class PostDetailView(DetailView): 
     model = Post
+    template_name = 'blog/post_detail.html'
 
 
 def about(request):
